b47254fd40eaaa5066d0c2d39b2e92f3bcc31974
Meetin82 <90614890+Meetin82@users.noreply.github.com>
2021-11-07 16:17:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = readNum("number n: ");
+        if (checkNumberPositive(n)){
+            System.out.println("Error. The number must be greater than or equal zero!");
+            return;
+        }
+
+        int numberOfTribonacci = countNumberOfTribonacci(n);
+
+        printNumberOfTribonacci(numberOfTribonacci);
+    }
+
+    private static int countNumberOfTribonacci(int n) {
+        int a = 0;
+        int b = 0;
+        int c = 1;
+        int result = 0;
+        for (int i = 0; i <= n;) {
+            if (n == 0){
+                break;
+            }
+            result = a + b + c;
+            i = result;
+            if (result >= n) {
+                break;
+            }
+            a = b;
+            b = c;
+            c = result;
+        }
+        return result;
+    }
+
+    private static boolean checkNumberPositive(int num) {
+        return num < 0;
+    }
+
+    private static void printNumberOfTribonacci(int numberOfTribonacci){
+        System.out.println("Smallest tribonacci number greater than or equal to n: " + numberOfTribonacci);
+    }
+
+    private static int readNum(String value) {
+        System.out.print("Enter " + value);
+        Scanner scn = new Scanner(System.in);
+        return scn.nextInt();
+    }
+}
